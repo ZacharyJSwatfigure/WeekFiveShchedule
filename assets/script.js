4558a2c4fdaa94e61a41288e81d42133bc50ecc4
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2021-11-17 17:16:34 -0700

    Finsihed layout for now finsihed css for now working on JS

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+let time8 = ""
+let time9 = ""
+let time10 = ""
+let time11 = ""
+let time12 = ""
+let time1 = ""
+let time2 = ""
+let time3 = ""
+let time4 = ""
+let time5 = ""
+
+
+const saveMe = function() {
+    document.getElementById("hour8").textContent
+    document.getElementById("hour9").textContent
+    document.getElementById("hour10").textContent
+    document.getElementById("hour11").textContent
+    document.getElementById("hour12").textContent
+    document.getElementById("hour1").textContent
+    document.getElementById("hour2").textContent
+    document.getElementById("hour3").textContent
+    document.getElementById("hour4").textContent
+    document.getElementById("hour5").textContent
+}
+
+
+document.getElementsByClassName("saveBtn").addEventListener("click", saveMe);
