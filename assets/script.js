855e2fd01e6100fe012b302ac04f2880940e5bd6
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2021-11-20 19:25:48 -0700

    trying to make my functions save to local storage part one

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,17 @@
 //intial global values. they arent set unless something saved in local storage
 
-var time8 = "";
-var time9 = "";
-var time10 = "";
-var time11 = "";
-var time12 = "";
-var time1 = "";
-var time2 = "";
-var time3 = "";
-var time4 = "";
-var time5 = "";
+const button = document.getElementByClass("saveBtn");
+
+var time8 = "" 
+var time9 = "" 
+var time10 = "" 
+var time11 = "" 
+var time12 = "" 
+var time1 = "" 
+var time2 = "" 
+var time3 = "" 
+var time4 = "" 
+var time5 = "" 
 //This decides if something was saved previously in local storage be being set to true (for when something was saved) 
 //and false (for when nothing was saved)
 var savedData = localStorage.getItem("isThereSavedData");
@@ -45,21 +47,25 @@ window.onload = function(){
 
 
 //this function is called when the saved button is clicked
-//it takes the input from all time slots and saves them to their own local storage
+//it takes the input from all time slots ans saves them to the correspionging time variable 
 const saveMe = function() {
     //here it sets a variable to be equal to the text held within that time slots input
-    time8 = document.getElementById("hour8").textContent;
-    time9 = document.getElementById("hour9").textContent;
-    time10 = document.getElementById("hour10").textContent;
-    time11 = document.getElementById("hour11").textContent;
-    time12 = document.getElementById("hour12").textContent;
-    time1 =  document.getElementById("hour1").textContent;
-    time2 =  document.getElementById("hour2").textContent;
-    time3 = document.getElementById("hour3").textContent;
-    time4 = document.getElementById("hour4").textContent;
-    time5 = document.getElementById("hour5").textContent;
+    time8 = document.getElementById("hour8").value;
+    time9 = document.getElementById("hour9").value;
+    time10 = document.getElementById("hour10").value;
+    time11 = document.getElementById("hour11").value;
+    time12 = document.getElementById("hour12").value;
+    time1 =  document.getElementById("hour1").value;
+    time2 =  document.getElementById("hour2").value;
+    time3 = document.getElementById("hour3").value;
+    time4 = document.getElementById("hour4").value;
+    time5 = document.getElementById("hour5").value;
 
+    saveToLoc()
+}
 
+const saveToLoc = function(){
+    //setting local storage to equal the value currently held within the variable time
     localStorage.setItem("theEight", time8);
     localStorage.setItem("theNine", time9);
     localStorage.setItem("theTen", time10);
@@ -72,9 +78,7 @@ const saveMe = function() {
     localStorage.setItem("theFive", time5);
 
     localStorage.setItem("isThereSavedData", true)
-
-
 }
 
 
-document.getElementsByClassName("saveBtn").addEventListener("Click", saveMe);
+button.addEventListener("click", saveMe)
